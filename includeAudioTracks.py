9066ffee72c8362b73eae876642c2c3b78ca1e39
Ivan Sukhikh <ivan.s@mightybuildings.com>
2020-12-05 18:28:56 +0300

    audio tracks merger searches audio files in subfolders

--- includeAudioTracks.py
+++ includeAudioTracks.py
@@ -1,14 +1,37 @@
-from os import walk,system
+from pathlib import Path
 
-mypath="D:\Downloads\Fullmetal Alchemist Brotherhood [BDRip] [1080p]\\"
-files = []
-for (_, _, filenames) in walk(mypath):
-    files.extend(filenames)
+mypath="D:\Downloads\[Beatrice-Raws] Overlord [BDRip 1920x1080 x264 FLAC]\\"
+    
+videoFiles=list(Path(mypath).glob("*.[mM][kK][vV]"))
+audioFiles=list(Path(mypath).rglob("*.[mM][kK][aA]"))
 
-audio = filter(lambda file: file.endswith('.mka'), files)
-audio=list(audio)
-video = filter(lambda file: file.endswith('.mkv'), files)
-video=list(video)
+from os.path import splitext, basename
 
-for i, file in enumerate(video):
-  system(f"ffmpeg -i \"{mypath}{file}\" -i \"{mypath}{audio[i]}\" -map 0 -map 1:a -c copy \"{mypath}output_{file}\"")
+audioTracks=[]
+
+class Video:
+    def __init__(self, VideoFile):
+        self.VideoFile = VideoFile
+        self.AudioFiles = []
+
+videos=[]
+for i, videoFile in enumerate(videoFiles):
+    videoFileName=splitext(basename(videoFile))[0]
+    video=Video(videoFile)
+    videos.append(video)
+    for j, audioFile in enumerate(audioFiles):
+        audioFileName=splitext(basename(audioFile))[0]
+        if videoFileName==audioFileName:
+            video.AudioFiles.append(audioFile)
+
+from os import system
+
+for video in videos:
+    command=f"ffmpeg -i \"{video.VideoFile}\" "  
+    for i, audio in enumerate(video.AudioFiles):
+         command+= f"-i \"{audio}\" "
+    command+="-map 0 "
+    for i, audio in enumerate(video.AudioFiles):
+        command+=f"-map {i+1}:a -metadata:s:a:{i+1} language=ru -metadata:s:a:{i+1} title=\"{audio.parts[-2]}\" " 
+    command+=f"-c copy \"{mypath}output_{basename(video.VideoFile)}\""
+    system(command)
